96f3f0f1e62e120ff47baf45d861617823d30c70
dmfxyz <d@dmf.xyz>
2022-06-02 22:33:46 -0400

    add a couple more tests

--- test/rpc.test.js
+++ test/rpc.test.js
@@ -1,5 +1,8 @@
-import { jest } from '@jest/globals';
+import { jest } from '@jest/globals'
 import { rpc } from "../rpc"
+import fetch from 'node-fetch'
+
+const dummy_url = "https://myrpc.com/"
 beforeEach(() => {
     jest.clearAllMocks()
 })
@@ -12,6 +15,29 @@ test('dummy test', () => {
 
 test('get block', () => {
     rpc.makeRPC = jest.fn()
-    rpc.getBlock("https://myrpc.com/", '10')
-    expect(rpc.makeRPC).toHaveBeenCalledWith("https://myrpc.com/", "eth_getBlockByNumber", ['10', false])
+    rpc.getBlock(dummy_url, '10')
+    expect(rpc.makeRPC).toHaveBeenCalledWith(dummy_url, "eth_getBlockByNumber", ['10', false])
+})
+
+test('get past events', () => {
+    rpc.makeRPC = jest.fn()
+    let address, topics = ['0x' + '00'.repeat(20), ["a", "b"]]
+    rpc.getPastEvents(dummy_url, address, topics)
+    expect(rpc.makeRPC).toHaveBeenCalledWith(dummy_url, "eth_getLogs", [{
+        address: address,
+        topics: topics,
+        fromBlock: '0xe02db4',
+        toBlock: 'latest'
+    }])
+})
+
+test('get storage', () => {
+    rpc.makeRPC = jest.fn()
+    let address, slot = ['0x' + '00'.repeat(20), 0]
+    rpc.RPCGetStorage(dummy_url, address, slot)
+    expect(rpc.makeRPC).toHaveBeenCalledWith(
+        dummy_url,
+        "eth_getStorageAt",
+        [address, slot, 'latest']
+    )
 })
