856334a75ba75b8b0ccb6d7c40c57ecf1c4c077f
dmfxyz <d@dmf.xyz>
2022-06-04 23:35:32 -0400

    proper async setup/teardown and tests

--- test/locktopus.test.js
+++ test/locktopus.test.js
@@ -1,20 +1,27 @@
 import { jest } from '@jest/globals'
 import { locktopus } from '../locktopus.js'
-import * as rpc from '../rpc.js'
+import { dummy_url, dummy_trace } from './test-constants.js'
+import { rpc } from '../rpc.js'
+import lib from '../lib/dmap/dmap.js'
+import fs from 'fs'
 
-const dummy_url = "https://myrpc.com/"
-rpc.rpc.makeRPC = jest.fn()
+rpc.makeRPC = jest.fn()
 
 beforeEach(() => {
     locktopus.set_config({'eth_rpc': dummy_url, 'finality': '60'})
+    return locktopus.init_db('./testdb')
 })
 
-test('test init last', () => {
-    rpc.rpc.makeRPC.mockReturnValueOnce({"timestamp": '1'})
-    const getBlock = jest.spyOn(rpc.rpc, 'getBlock')
-    locktopus.init_last()
+afterEach(() => {
+    return Promise.all([locktopus.close(),  fs.promises.rmdir('./testdb', {recursive: true, force: true})])
+})
+
+test('test init last', async () => {
+    rpc.makeRPC.mockReturnValueOnce({"timestamp": '1'})
+    const getBlock = jest.spyOn(rpc, 'getBlock')
+    await locktopus.init_last()
     expect(getBlock).toHaveBeenCalledWith(dummy_url, 'latest')
-    expect(rpc.rpc.makeRPC).toHaveBeenCalledWith(
+    expect(rpc.makeRPC).toHaveBeenCalledWith(
         dummy_url,
         "eth_getBlockByNumber",
         ['latest', false]
--- test/rpc.test.js
+++ test/rpc.test.js
@@ -1,28 +1,22 @@
 import { jest } from '@jest/globals'
 import { rpc } from "../rpc"
+import { dummy_url } from './test-constants'
 import fetch from 'node-fetch'
 
-const dummy_url = "https://myrpc.com/"
 beforeEach(() => {
     jest.clearAllMocks()
 })
 
-test('dummy test', () => {
-    rpc.makeRPC = jest.fn(() => "locktopus");
-    const result = rpc.makeRPC(null, null, null)
-    expect(result).toBe('locktopus')
-})
-
-test('get block', () => {
+test('get block', async () => {
     rpc.makeRPC = jest.fn()
-    rpc.getBlock(dummy_url, '10')
+    await rpc.getBlock(dummy_url, '10')
     expect(rpc.makeRPC).toHaveBeenCalledWith(dummy_url, "eth_getBlockByNumber", ['10', false])
 })
 
-test('get past events', () => {
+test('get past events', async () => {
     rpc.makeRPC = jest.fn()
     let address, topics = ['0x' + '00'.repeat(20), ["a", "b"]]
-    rpc.getPastEvents(dummy_url, address, topics)
+    await rpc.getPastEvents(dummy_url, address, topics)
     expect(rpc.makeRPC).toHaveBeenCalledWith(dummy_url, "eth_getLogs", [{
         address: address,
         topics: topics,
@@ -31,10 +25,10 @@ test('get past events', () => {
     }])
 })
 
-test('get storage', () => {
+test('get storage', async () => {
     rpc.makeRPC = jest.fn()
     let address, slot = ['0x' + '00'.repeat(20), 0]
-    rpc.RPCGetStorage(dummy_url, address, slot)
+    await rpc.RPCGetStorage(dummy_url, address, slot)
     expect(rpc.makeRPC).toHaveBeenCalledWith(
         dummy_url,
         "eth_getStorageAt",
